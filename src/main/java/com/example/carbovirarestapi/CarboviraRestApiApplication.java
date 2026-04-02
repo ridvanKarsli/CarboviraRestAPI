a5989287fe0ac7c97cbb27da415887d6bb06329a
ridvanKarsli <rdvn.35050@gmail.com>
2026-04-02 09:32:48 +0300

    Started the project

---
+++ src/main/java/com/example/carbovirarestapi/CarboviraRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.carbovirarestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarboviraRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarboviraRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/carbovirarestapi/CarboviraRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carbovirarestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarboviraRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
